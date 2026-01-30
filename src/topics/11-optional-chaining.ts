d8651c328fbcda08c26a219bfd3899bbcca3fe2c
Diego Ricaurte <daricaurte.ext@gmv.com>
2026-01-29 22:09:14 -0500

    Encadenamiento opcional

---
+++ src/topics/11-optional-chaining.ts
@@ -0,0 +1,24 @@
+export interface Passenger {
+  name: string;
+  children?: string[];
+}
+
+const passenger1: Passenger = {
+  name: "Diego",
+};
+
+const passenger2: Passenger = {
+  name: "Channia",
+  children: ["Bela", "Isaac"],
+};
+
+const returnChildrenNumber = (passenger: Passenger): number => {
+  const howManyChildren = passenger.children?.length ?? 0;
+  //   const howManyChildren = passenger.children!.length;
+
+  console.log(passenger.name, howManyChildren);
+
+  return howManyChildren;
+};
+
+returnChildrenNumber(passenger1);
